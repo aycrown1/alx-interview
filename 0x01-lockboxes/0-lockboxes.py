5204034df99b6d5b8554c0afe9cc7cc968eb535a
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-12 01:51:11 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module is an interview challenge in python
+"""
+
+def canUnlockAll(boxes):
+    """
+    canUnlockAll function determines if all the boxes can be opened
+    """
+    keys = set([0])
+    opened_boxes = set()
+
+    while keys:
+        current_key = keys.pop()
+        opened_boxes.add(current_key)
+
+        current_box = boxes[current_key]
+        keys.update(set(current_box) - opened_boxes)
+
+    return len(opened_boxes) == len(boxes)
