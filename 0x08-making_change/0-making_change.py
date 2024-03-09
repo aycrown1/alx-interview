b41fdef3bdfa2ba322e3faad0c9017b89d294cbd
aycrown1 <ayoadeadedamola198@gmail.com>
2024-03-08 19:42:51 -0800

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Interview question about determining the fewest number of coins needed
+    to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Function that determine fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+        if total != 0:
+            return -1
+        return change
