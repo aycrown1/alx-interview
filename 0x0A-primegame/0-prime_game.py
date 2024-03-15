88feaa6d77ece3cc70c6e6991c9a9edc7186b7bf
aycrown1 <ayoadeadedamola198@gmail.com>
2024-03-14 18:02:34 -0700

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,30 +3,30 @@
 
 
 def isWinner(x, nums):
-   """
-   This function determines who the winner of each game is.
-   """
-   if not nums or x < 1:
-       return None
-   max_num = max(nums)
-
-   sieve = [True for _ in range(max(max_num + 1, 2))]
-   for i in range(2, int(pow(max_num, 0.5)) + 1):
-       if not sieve[i]:
-           continue
-       for j in range(i * i, max_num + 1, i):
-           sieve[j] = False
-   sieve[0] = sieve[1] = False
-   y = 0
-   for i in range(len(sieve)):
-       if sieve[i]:
-           y += 1
-       sieve[i] = y
-   player1 = 0
-   for x in nums:
-       player1 += sieve[x] % 2 == 1
-   if player1 * 2 == len(nums):
+    """
+    This function determines who the winner of each game is.
+    """
+    if not nums or x < 1:
         return None
-   if player1 * 2 > len(nums):
-       return "Maria"
-   return "Ben"
+    max_num = max(nums)
+
+    sieve = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            sieve[j] = False
+    sieve[0] = sieve[1] = False
+    y = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            y += 1
+            sieve[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += sieve[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
